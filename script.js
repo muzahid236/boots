b4a7286fb98992241b867e05eae541fc4ed2552b
Muzahid <takiajannat920@gmail.com>
2024-12-31 20:51:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-// CALCULATOR PROGRAM
+const display = document.getElementById("display");
 
-const display = document.getElementById('display');
-
-function appendToDisplay(input){
-     display.value += input;
+function appendtoDisplay(input) {
+  display.value += input;
 }
 
-function clearDiplay(){
-    display.value = "";
+function clearDisplay() {
+  display.value = "";
 }
 
-function calculate(){
-     try{
-         display.value = eval(display.value);
-     }
-     catch(error){
-        display.value = "Error";
-     }
-     
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
